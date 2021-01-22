cd0ce22925da2437b0ae63a012ff1ef24e29e315
Gary Kono <gary.a.kono@gmail.com>
2021-01-21 17:04:04 -0800

    Altered index, game engin, and main for solo assignment

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./sprites/owl.png");
+
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
 
 	gameEngine.init(ctx);
+
+	gameEngine.addEntity(new Owl(gameEngine, 50, 50, ASSET_MANAGER.getAsset("./sprites/owl.png")));
 
 	gameEngine.start();
 });
